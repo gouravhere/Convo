e761ed76d60860bd1f9ce9258cb1bf3ea5e6ffb4
gourav sharma <gourav.101sharma@gmail.com>
2025-07-13 22:24:09 +0530

    updated version of react router

--- chat-app/chat-app-backend/src/main/java/com/substring/chat/controllers/RoomController.java
+++ chat-app/chat-app-backend/src/main/java/com/substring/chat/controllers/RoomController.java
@@ -1,5 +1,6 @@
 package com.substring.chat.controllers;
 
+import com.substring.chat.config.AppConstants;
 import com.substring.chat.entities.Message;
 import com.substring.chat.entities.Room;
 import com.substring.chat.repositories.RoomRepository;
@@ -16,7 +17,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/rooms")
-@CrossOrigin("http://localhost:5173")
+@CrossOrigin(AppConstants.FRONT_END_BASE_URL)
 public class RoomController {
 
     private RoomRepository roomRepository;
